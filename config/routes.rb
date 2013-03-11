15587f8e6c99543491943d69a57f36de9646be96
Josh Schramm <josh.schramm@gmail.com>
2013-03-11 00:33:08 -0400

    fixed update and delet

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Tribute::Application.routes.draw do
-  resources :pages, :except => [:show]
+  resources :pages, :except => [:show, :update, :destroy]
 
   devise_for :users,
     controllers: {
@@ -20,4 +20,6 @@ Tribute::Application.routes.draw do
   root :to => 'home#index'
 
   get ":id", to: 'pages#show', as: "page"
+  put ":id", to: 'pages#update', as: "page"
+  delete ":id", to: 'pages#destroy', as: "page"
 end
