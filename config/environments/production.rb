0a21f0fc6d9c29e88007e42b09e371192687e0b5
Josh Schramm <josh.schramm@gmail.com>
2013-03-10 20:30:59 -0400

    moved asset precompile directive

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,4 @@ Tribute::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { :host => 'tributesite.com' }
-  config.assets.initialize_on_precompile = false
 end
